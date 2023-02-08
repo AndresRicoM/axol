82eb6b63210dc8bb98825fee2ab5c5bc656b4315
AndresRicoM <aricom@mit.edu>
2023-02-08 15:51:38 -0500

    added th sensor program.

--- sensors/environmental_sensor/code/th_axol.ino
+++ sensors/environmental_sensor/code/th_axol.ino
@@ -22,8 +22,7 @@
              |' | |.    |    ||       | |   |  |    ||      |
           ___|  '-'     '    ""       '-'   '-.'    '`      |____
 
-   Code for temperature and humidity sensor for the Axol sensing system. 
-   The code runs on an ESP32 chip along with a STHx sensor module. 
+   Code for temperature and humidity sensor for the Axol sensing system. The code runs on an ESP32 chip along with a STHx sensor module. 
 
    Andres Rico - aricom@mit.edu
   
@@ -47,10 +46,10 @@
 #define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
 #define TIME_TO_SLEEP  10        /* Time ESP32 will go to sleep (in seconds) */
 
-uint8_t broadcastAddress[] = {0xXX, 0xXX, 0xXX, 0xXX, 0xXX, 0xXX}; //Replace XX with Paired Homehub Address MAC Address digits. 
+uint8_t broadcastAddress[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //Replace XX with Paired Homehub Address MAC Address digits. 
 constexpr char WIFI_SSID[] = ""; //Input WIFI network name.
 
-int32_t getWiFiChannel(const char *ssid) { //Function for 
+int32_t getWiFiChannel(const char *ssid) {
 
   if (int32_t n = WiFi.scanNetworks()) {
     for (uint8_t i = 0; i < n; i++) {
@@ -63,16 +62,16 @@ int32_t getWiFiChannel(const char *ssid) { //Function for
   return 0;
 }
 
-typedef struct struct_message { //Structure of sensor data packet that is sent to the HomeHub. 
-  char id[50];    //Sensor MAC Address
-  int type;       //Type of sensor. -> Temperature/Humidity = 3
-  int temp;       //Current temperature
-  int humidity;   //Current humidity
+typedef struct struct_message {
+  char id[50];
+  int type;
+  int temp;
+  int humidity;
 } struct_message;
 
 struct_message myData;
 
-String address = WiFi.macAddress(); //Fetch sensor MAC Address. 
+String address = WiFi.macAddress();
 char mac_add[50];
 
 int attempts = 0;
@@ -86,19 +85,18 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 
 SensirionI2CSht4x sht4x;
 
-int sensPin = 4; /Pin for powering sensor from ESP32 GPIO. 
+int sensPin = 4;
+//int lightPin = 5;
 float temperature;
 float humidity;
 
 void getHumTemp() {
-  
   uint16_t error;
   char errorMessage[256];
 
   delay(1000);
 
   error = sht4x.measureHighPrecision(temperature, humidity);
-  
   if (error) {
     Serial.print("Error trying to execute measureHighPrecision(): ");
     errorToString(error, errorMessage, 256);
@@ -129,6 +127,7 @@ void setup() {
 
   getHumTemp();
   address.toCharArray(mac_add, 50);
+  Serial.println(mac_add);
   WiFi.mode(WIFI_STA);
   int32_t wifi_channel = getWiFiChannel(WIFI_SSID);
   strcpy(myData.id, mac_add);
@@ -145,10 +144,12 @@ void setup() {
   //WiFi.printDiag(Serial); // Uncomment to verify channel change after
 
   //Serial.println(WiFi.macAddress());
-  // Init ESP-NOW
-  esp_now_init(); //Start ESP now communication. 
-  
-  esp_now_register_send_cb(OnDataSent); // get the status of Trasnmitted packet
+  // Init ESP-NOWm
+  esp_now_init();
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
 
   // Register peer
   esp_now_peer_info_t peerInfo;
@@ -156,16 +157,16 @@ void setup() {
   peerInfo.encrypt = false;
 
   // Add peer
-  esp_now_add_peer(&peerInfo); //Loof for esp now peer. 
+  esp_now_add_peer(&peerInfo);
 
   // Send message via ESP-NOW
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
 
-  esp_sleep_enable_timer_wakeup(43200000000) ; //Time in uS -> Reports Twice a day. 
+  esp_sleep_enable_timer_wakeup(43200000000) ; //TIME_TO_SLEEP * uS_TO_S_FACTOR); //Twice per day. 86400000000); 43200000000
 
-  esp_wifi_stop(); //Shut down wifi before going to sleep. 
+  esp_wifi_stop();
 
-  esp_deep_sleep_start(); //Go into Deep Sleep
+  esp_deep_sleep_start();
 
 }
 
